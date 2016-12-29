ac344632463fe743c1b504c5a068f85573fc966c
Zhang Yiming <zhangyiming@zhangs-mbp.cn.ibm.com>
2016-12-29 18:14:33 +0800

    1. Fix defects; 2. Add stop_monitor_connection function

--- proxy.plugin.zsh
+++ proxy.plugin.zsh
@@ -4,8 +4,8 @@ proxy_password=""
 proxy_local_port=""
 proxy_configured="False"
 proxy_execpath=$ZSH_CUSTOM"/plugins/proxy"
-proxy_statusfile=$TMPDIR/status.tmp
-proxy_pidfile=$TMPDIR/omz_proxy.pid
+proxy_statusfile=${TMPDIR}status.tmp
+proxy_pidfile=${TMPDIR}omz_proxy.pid
 
 autoload _check_connection
 autoload _monitor_connection
@@ -57,9 +57,33 @@ function check_connection() {
     return $?
 }
 
-# Start a daemon process to monitor connection, re-connect to proxy when it's broken
-function monitor_connection() {
-    _monitor_connection $proxy_pidfile &
+# Start a daemon process to monitor connection
+function start_monitor_connection() {
+    if [ -e $proxy_pidfile ]; then
+        # Find out if there's already a monitor thread available.
+        oldpid=`cat $proxy_pidfile` 
+        ret=`ps ax -o pid | grep $oldpid | wc -l`
+        [ $ret -eq 1 ] && return 0
+    fi
+
+    _monitor_connection & bg_pid=$!
+    echo $bg_pid > $proxy_pidfile
+
+    return 0
+}
+
+# Stop the daemon process
+function stop_monitor_connection() {
+    if [ -e $proxy_pidfile ]; then
+        oldpid=`cat $proxy_pidfile`
+        ps ax -o pid,ppid | grep $oldpid | awk '{print $1}' | xargs kill -s INT
+        rm -f $proxy_pidfile
+    else
+        # NO pidfile means no process to kill
+        return 1
+    fi
+
+    return 0
 }
 
 read_proxy_config

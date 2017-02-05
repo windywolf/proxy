ce8216b1ce9317c255a035a80cc7b274a7d0d87a
Zhang Yiming <zhangyiming@192.168.2.120>
2017-02-05 22:15:08 +0800

    1. Add disconnect function; 2. modify timeout constant in check_connection function; 3. Fix defect.

--- proxy.plugin.zsh
+++ proxy.plugin.zsh
@@ -8,7 +8,6 @@ proxy_execpath=$ZSH_CUSTOM"/plugins/proxy"
 proxy_statusfile=${TMPDIR}status.tmp
 proxy_pidfile=${TMPDIR}omz_proxy.pid
 proxy_mux_uds="${HOME}/.ssh/proxy.sock"
-proxy_debug_output=""
 
 autoload _check_connection
 autoload _monitor_connection
@@ -26,7 +25,6 @@ function read_proxy_config() {
     proxy_password=`sed -n "s/password=//p" $proxy_execpath/proxy.conf`
     proxy_local_port=`sed -n "s/local_port=//p" $proxy_execpath/proxy.conf`
     proxy_remote_port=`sed -n "s/remote_port=//p" $proxy_execpath/proxy.conf`
-    debug_switch=`sed -n "s/debug=//p" $proxy_execpath/proxy.conf`
 
     proxy_configured="True"
 }
@@ -54,6 +52,31 @@ function connect_proxy() {
     expect $proxy_execpath/connect_to_server.exp $proxy_proxy_server $proxy_user_name $proxy_password $proxy_local_port $proxy_remote_port $proxy_mux_uds >/dev/null & 
 
     return 0
+}
+
+# Disconnect the connection to proxy
+function disconnect_proxy() {
+    # 0. Check if the proxy info's already configured
+    if [ $proxy_configured != "True" ]; then
+        echo "Please config proxy.conf in ${proxy_execpath} first!"
+        return 1
+    fi
+    # 1. Check if we are already connected to the proxy
+    check_connection
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        echo "The proxy server is already disconnected!"
+        return 0
+    fi
+    # 2. Try to disconnect the connection by killing the background multiplex SSH process
+    mux_pid=`ps aux | grep $proxy_mux_uds | grep -v 'grep' | awk '{print $2}'`
+    kill $mux_pid
+    check_connection
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        echo "The proxy server's disconnected!"
+        return 0
+    fi
 }
 
 # Check if the connection to proxy is still available
